a219b7dcf8cb3d08d26ab8abd416d095fb08b65c
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-05 22:30:28 +0100

    feat: add db connection test method

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,31 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (protected val dbConfigProvider: DatabaseConfigProvider,
+                                cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  import profile.api._
+
+  def testDatabaseConnection() = Action.async { implicit request =>
+    // Exemple de requête pour tester la connexion à la base de données
+    val query = sql"SELECT 1".as[Int]
+
+    db.run(query).map { result =>
+      Ok(s"Connexion à la base de données réussie. Résultat de la requête : $result")
+    }.recover {
+      case ex: Exception =>
+        InternalServerError(s"Erreur lors de la connexion à la base de données : ${ex.getMessage}")
+    }
+  }
+
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
