8cdfbd94e6ea3e1be380e22bc994783308ffc275
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-09 22:53:03 +0100

    feat: add missing models

--- app/models/Tournee.scala
+++ app/models/Tournee.scala
@@ -2,21 +2,23 @@ package models
 
 import play.api.libs.json.{Json, OFormat}
 
+import java.time.LocalDate
+
 case class Tournee(id_tournee: Long,
-                   date: String,
-                   livreur: Long,
-                   nom: String,
+                   date: LocalDate,
+                   id_livreur: Option[Long],
+                   nom: Option[String],
                    statut: String)
 object Tournee {
   implicit val format: OFormat[Tournee] = Json.format[Tournee]
 }
 
-case class TourneeCreation(date: String)
+case class TourneeCreation(date: LocalDate)
 object TourneeCreation {
   implicit val format: OFormat[TourneeCreation] = Json.format[TourneeCreation]
 }
 
-case class TourneeUpdate(nom: String,
+case class TourneeUpdate(nom: Option[String],
                          statut: String)
 object TourneeUpdate {
   implicit val format: OFormat[TourneeUpdate] = Json.format[TourneeUpdate]
@@ -27,14 +29,21 @@ object TourneeResume {
   implicit val format: OFormat[TourneeResume] = Json.format[TourneeResume]
 }
 
-case class Resume(article: Article,
+case class Resume(article: ArticleSansPourcentage,
                   nb_caisses: Int,
                   nb_unites: Int)
 object Resume {
   implicit val format: OFormat[Resume] = Json.format[Resume]
 }
 
-case class LivreurModification(id_livreur: Long)
+case class LivreurModification(id_livreur: Option[Long])
 object LivreurModification {
   implicit val format: OFormat[LivreurModification] = Json.format[LivreurModification]
+}
+
+case class ArticleSansPourcentage(id_article: Long,
+                   libelle: String,
+                   taille: Option[String])
+object ArticleSansPourcentage {
+  implicit val format: OFormat[ArticleSansPourcentage] = Json.format[ArticleSansPourcentage]
 }
