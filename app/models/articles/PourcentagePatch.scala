2613f179f6e77959c09996c62adfcfa0c093bc9b
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-09 13:52:00 +0100

    feat: add PourcentagePatch model

--- app/models/articles/PourcentagePatch.scala
+++ app/models/articles/PourcentagePatch.scala
@@ -1,9 +1,3 @@
 package models.articles
 
-import play.api.libs.json.{Json, OFormat}
-
 case class PourcentagePatch(new_pourcentage: Int)
-
-object PourcentagePatch {
-  implicit val format: OFormat[PourcentagePatch] = Json.format[PourcentagePatch]
-}
