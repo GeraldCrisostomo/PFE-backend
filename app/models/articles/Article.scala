49f65d2af40b1755ab1bf474eaa74ff3d4505b21
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-09 13:50:43 +0100

    feat: add Article model

--- app/models/articles/Article.scala
+++ app/models/articles/Article.scala
@@ -1,12 +1,6 @@
 package models.articles
 
-import play.api.libs.json.{Json, OFormat}
-
 case class Article(id_article: Long,
                    libelle: String,
-                   taille: Option[String],
+                   taille: String,
                    pourcentage: Int)
-
-object Article {
-  implicit val format: OFormat[Article] = Json.format[Article]
-}
