f4180054b3720b199221df97c1607f9f3b24e937
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-09 15:31:07 +0100

    fix: enable serialization

--- app/models/articles/PourcentagePatch.scala
+++ app/models/articles/PourcentagePatch.scala
@@ -1,3 +1,9 @@
 package models.articles
 
+import play.api.libs.json.{Json, OFormat}
+
 case class PourcentagePatch(new_pourcentage: Int)
+
+object PourcentagePatch {
+  implicit val format: OFormat[PourcentagePatch] = Json.format[PourcentagePatch]
+}
