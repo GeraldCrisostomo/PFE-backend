fa98a875692d2d6b01f19c0c3afbda4a11e81e87
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-11 19:38:21 +0100

    feat: add ArticleSansPourcentage

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -2,15 +2,30 @@ package models
 
 import play.api.libs.json.{Json, OFormat}
 
-case class Article(id_article: Long,
-                   libelle: String,
-                   taille: Option[String],
-                   pourcentage: Int)
+case class Article(
+                    id_article: Long,
+                    libelle: String,
+                    taille: Option[String],
+                    pourcentage: Int
+                  )
+
+case class ArticleSansPourcentage(
+                                    id_article: Long,
+                                    libelle: String,
+                                    taille: Option[String]
+                                 )
+
+case class PourcentagePatch(
+                             new_pourcentage: Int
+                           )
+
 object Article {
   implicit val format: OFormat[Article] = Json.format[Article]
 }
 
-case class PourcentagePatch(new_pourcentage: Int)
+object ArticleSansPourcentage {
+  implicit val format: OFormat[ArticleSansPourcentage] = Json.format[ArticleSansPourcentage]
+}
 
 object PourcentagePatch {
   implicit val format: OFormat[PourcentagePatch] = Json.format[PourcentagePatch]
