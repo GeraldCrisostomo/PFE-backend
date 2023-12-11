88bdd5281e1d0c5bd8e2f197d38751bdff5826f6
louisvanbel <louis.vanbel@student.vinci.be>
2023-12-11 22:08:26 +0100

    feat: add LigneCommandeParDefautWithDetails

--- app/models/LigneCommandeParDefaut.scala
+++ app/models/LigneCommandeParDefaut.scala
@@ -18,7 +18,27 @@ case class LigneCommandeParDefaut(
                                    nb_unites: Int
                                  )
 
+/**
+ * Représente une ligne de commande par défaut avec des détails supplémentaires, y compris l'article sans pourcentage associé.
+ *
+ * @param id_creche      L'identifiant unique de la crèche associée à la ligne de commande par défaut.
+ * @param article        L'article sans pourcentage associé à la ligne de commande par défaut.
+ * @param nb_caisses     Le nombre de caisses dans la ligne de commande par défaut.
+ * @param nb_unites      Le nombre d'unités dans la ligne de commande par défaut.
+ */
+case class LigneCommandeParDefautWithDetails(
+                                               id_creche: Long,
+                                               article: ArticleSansPourcentage,
+                                               nb_caisses: Int,
+                                               nb_unites: Int
+                                            )
+
 object LigneCommandeParDefaut {
   // Implémente le format JSON implicite pour la sérialisation/désérialisation des objets LigneCommandeParDefaut.
   implicit val format: OFormat[LigneCommandeParDefaut] = Json.format[LigneCommandeParDefaut]
+}
+
+object LigneCommandeParDefautWithDetails {
+  // Implémente le format JSON implicite pour la sérialisation/désérialisation des objets LigneCommandeParDefautWithDetails.
+  implicit val format: OFormat[LigneCommandeParDefautWithDetails] = Json.format[LigneCommandeParDefautWithDetails]
 }
