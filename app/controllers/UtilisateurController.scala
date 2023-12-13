631d5b8dc1df8e49cd4cd65839e31c69804c7565
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-12-13 15:54:48 +0100

    feat: add missing Utilisateur controller methods

--- app/controllers/UtilisateurController.scala
+++ app/controllers/UtilisateurController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import models.{ConnexionInfo, UtilisateurCreate, UtilisateurUpdate}
+
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
@@ -7,27 +9,98 @@ import services.UtilisateurService
 
 import scala.concurrent.ExecutionContext
 
+/**
+ * Contrôleur pour gérer les opérations liées aux utilisateurs.
+ *
+ * @param cc                  Composants du contrôleur fournis par Play Framework.
+ * @param utilisateurService  Service fournissant les opérations liées aux utilisateurs.
+ * @param ec                  Contexte d'exécution nécessaire pour les opérations asynchrones.
+ */
 @Singleton
 class UtilisateurController @Inject()(cc: ControllerComponents, utilisateurService: UtilisateurService)(implicit ec: ExecutionContext)
   extends AbstractController(cc) {
 
-  def getAllUtilisateurs: Action[AnyContent] = Action.async { implicit request =>
-    utilisateurService.getAllUtilisateurs.map(articles => Ok(Json.toJson(articles)))
+  /**
+   * Récupère tous les utilisateurs.
+   *
+   * @return Action[AnyContent] Action résultant en la liste des utilisateurs au format JSON.
+   */
+  def getAllUtilisateurs: Action[AnyContent] = Action.async { implicit _ =>
+    utilisateurService.getAllUtilisateurs.map(utilisateurs => Ok(Json.toJson(utilisateurs)))
   }
 
-  def getAllUtilisateursByRole(role: String): Action[AnyContent] = Action.async { implicit request =>
-    utilisateurService.getAllUtilisateursByRole(role).map { supplements => Ok(Json.toJson(supplements))
+  /**
+   * Récupère tous les utilisateurs d'un rôle spécifique.
+   *
+   * @param role Le rôle des utilisateurs à récupérer.
+   * @return Action[AnyContent] Action résultant en la liste des utilisateurs au format JSON.
+   */
+  def getAllUtilisateursByRole(role: String): Action[AnyContent] = Action.async { implicit _ =>
+    utilisateurService.getAllUtilisateursByRole(role).map { utilisateurs =>
+      Ok(Json.toJson(utilisateurs))
     }.recover {
-      case e: Exception => InternalServerError(s"An error occured ${e.getMessage} ")
+      case e: Exception => InternalServerError(s"Une erreur s'est produite : ${e.getMessage}")
     }
   }
 
-  def getUtilisateur(id_utilisateur: Long): Action[AnyContent] = Action.async { implicit request =>
-    utilisateurService.getUtilisateur(id_utilisateur).map { supplements => Ok(Json.toJson(supplements))
+  /**
+   * Récupère les informations d'un utilisateur spécifique.
+   *
+   * @param id_utilisateur L'identifiant unique de l'utilisateur à récupérer.
+   * @return Action[AnyContent] Action résultant en les informations de l'utilisateur au format JSON.
+   */
+  def getUtilisateur(id_utilisateur: Long): Action[AnyContent] = Action.async { implicit _ =>
+    utilisateurService.getUtilisateur(id_utilisateur).map { utilisateur =>
+      Ok(Json.toJson(utilisateur))
     }.recover {
-      case e: Exception => InternalServerError(s"An error occured ${e.getMessage} ")
+      case e: Exception => InternalServerError(s"Une erreur s'est produite : ${e.getMessage}")
     }
   }
 
+  /**
+   * Crée un nouvel utilisateur.
+   *
+   * @return Action[UtilisateurCreate] Action résultant en la création de l'utilisateur.
+   */
+  def createUtilisateur: Action[UtilisateurCreate] = Action.async(parse.json[UtilisateurCreate]) { implicit request =>
+    utilisateurService.createUtilisateur(request.body).map(_ => Created)
+  }
+
+  /**
+   * Modifie les informations d'un utilisateur spécifique.
+   *
+   * @param idUtilisateur L'identifiant unique de l'utilisateur à mettre à jour.
+   * @return Action[UtilisateurUpdate] Action résultant en la mise à jour des informations de l'utilisateur.
+   */
+  def updateUtilisateur(idUtilisateur: Long): Action[UtilisateurUpdate] = Action.async(parse.json[UtilisateurUpdate]) { implicit request =>
+    utilisateurService.updateUtilisateur(idUtilisateur, request.body).map {
+      case true => Ok
+      case false => NotFound
+    }
+  }
+
+  /**
+   * Supprime un utilisateur spécifique.
+   *
+   * @param idUtilisateur L'identifiant unique de l'utilisateur à supprimer.
+   * @return Action[Any] Action résultant en la suppression de l'utilisateur.
+   */
+  def deleteUtilisateur(idUtilisateur: Long): Action[Any] = Action.async { implicit _ =>
+    utilisateurService.deleteUtilisateur(idUtilisateur).map {
+      case true => Ok
+      case false => NotFound
+    }
+  }
+
+  /**
+   * Connecte un utilisateur en vérifiant les informations d'identification.
+   *
+   * @return Action[ConnexionInfo] Action résultant en la connexion de l'utilisateur.
+   */
+  def connectUtilisateur: Action[ConnexionInfo] = Action.async(parse.json[ConnexionInfo]) { implicit request =>
+    utilisateurService.connectUtilisateur(request.body).map {
+      case Some(utilisateur) => Ok(Json.toJson(utilisateur))
+      case None => Unauthorized
+    }
+  }
 }
-
