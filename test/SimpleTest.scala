c01b900ce8edade4c19633f41fa90dbb9e8a157e
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-12-13 16:38:47 +0100

    feat: add SimpleTest

---
+++ test/SimpleTest.scala
@@ -0,0 +1,15 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+/**
+ * Classe de test simple avec un test réussi toujours.
+ */
+class SimpleTest extends AnyFunSuite {
+
+  /**
+   * Test : Toujours réussi.
+   * Vérifie si l'assertion `true` est vraie, ce qui réussit toujours.
+   */
+  test("Always succeeds") {
+    assert(condition = true)
+  }
+}
